e4db32c544b6b2213b00ac5db7916a7fceea7ac3
rjjfox <ryanfox212@gmail.com>
2021-01-29 00:44:41 +0000

    Replace navigation with new design
    JS functions added to create responsive functionality. Material theme
    used for some icons

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function openMenu() {
+  var overlay = document.getElementById("header__overlay");
+  var menu = document.getElementById("main-nav");
+  overlay.classList.toggle("is-visible");
+  menu.classList.toggle("is-open");
+}
