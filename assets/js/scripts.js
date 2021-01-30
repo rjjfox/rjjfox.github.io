828337b18afc4fd51ab28551061fe5835a682f0a
rjjfox <ryanfox212@gmail.com>
2021-01-30 12:20:42 +0000

    More efficient importing of JS files

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+---
+---
+{% include_relative tools/lazyload.min.js %}
+{% include_relative tools/medium-zoom.min.js %}
+{% include_relative tools/my-medium-zoom.js %}
+{% include_relative main.js %}
---
+++ assets/js/tools/my-medium-zoom.js
@@ -0,0 +1,3 @@
+mediumZoom(':not(a) > img', {
+    background: 'rgba(255, 255, 255, .9)'
+})
